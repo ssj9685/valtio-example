1653eca26fbba4d396d91f9af4b9ad17d116486c
shinseokjin <ssj9685@naver.com>
2024-02-14 02:35:05 +0900

    refactor: prettier

--- src/hooks/todo/useTodoFilter.ts
+++ src/hooks/todo/useTodoFilter.ts
@@ -3,21 +3,21 @@ import { FILTER, Todo } from "../../types/todo/todoType";
 import { globalTodoProxy } from "@/state/todo/todoState";
 
 export function useTodoFilter(params: { todos: Map<string, Todo> }) {
-    const { todos } = params;
-  
-    const todoSnapshot = useSnapshot(globalTodoProxy);
-  
-    const { filter } = todoSnapshot;
-  
-    const todosArray = [...todos.values()];
-  
-    if (filter === FILTER.all) {
-      return todosArray;
-    }
-  
-    if (filter === FILTER.completed) {
-      return todosArray.filter((todo) => todo.completed);
-    }
-  
-    return todosArray.filter((todo) => !todo.completed);
+  const { todos } = params;
+
+  const todoSnapshot = useSnapshot(globalTodoProxy);
+
+  const { filter } = todoSnapshot;
+
+  const todosArray = [...todos.values()];
+
+  if (filter === FILTER.all) {
+    return todosArray;
   }
+
+  if (filter === FILTER.completed) {
+    return todosArray.filter((todo) => todo.completed);
+  }
+
+  return todosArray.filter((todo) => !todo.completed);
+}
